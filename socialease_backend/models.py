9ea32b6ad4b1b07c8081b223a0a226e84789ffaa
Shanker Kabishan <163110494+Kabishan0613@users.noreply.github.com>
2025-04-04 13:08:09 +0530

    .

--- socialease_backend/models.py
+++ socialease_backend/models.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017")
+# Connect to MongoDB with updated parameters
+client = MongoClient("mongodb+srv://socialease:sgkl01031308@cluster0.yo2mm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true&tlsAllowInvalidCertificates=false")
 db = client["socialease"]
 
 # Define collections
